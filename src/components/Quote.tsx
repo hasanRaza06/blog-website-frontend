fe15ff9190e1b5e6c46fcd9cde8a6274f857869c
Hasan Raza <hasanraza102515209@gmail.com>
2024-07-06 16:52:05 +0530

    Error fixed

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Quote() {
   return (
     <div className="bg-slate-200 h-screen flex justify-center flex-col">
--- src/pages/BlogsFinal.tsx
+++ src/pages/BlogsFinal.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 import Blogs from '../components/Blog'
 import PostBar from '../components/PostBar'
